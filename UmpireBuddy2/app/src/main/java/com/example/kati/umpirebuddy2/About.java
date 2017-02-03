d5a904ec88246a6a9e9a5ff15d90825d159d15e0
kw531 <katiwilliams531@gmail.com>
2017-02-03 15:58:29 -0600

    A simple ToDo list and Lab 02
    Lab 02 added and a simple to-do list

---
+++ UmpireBuddy2/app/src/main/java/com/example/kati/umpirebuddy2/About.java
@@ -0,0 +1,16 @@
+package com.example.kati.umpirebuddy2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        Intent intent = getIntent();
+    }
+}
---
+++ UmpireBuddy2/app/src/main/java/com/example/kati/umpirebuddy2/MainActivity.java
@@ -0,0 +1,165 @@
+package com.example.kati.umpirebuddy2;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private int strike_count = 0;
+    private int ball_count = 0;
+    private int out_count=0;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        inflater.inflate(R.menu.reset,menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        readOuts();
+        updateOutCount();
+
+        View strikeIncrementButton = findViewById(R.id.btnStrike);
+        strikeIncrementButton.setOnClickListener(this);
+
+        View ballIncrementButton = findViewById(R.id.btnBall);
+        ballIncrementButton.setOnClickListener(this);
+
+        updateStrikeCount();
+        updateBallCount();
+    }
+
+    private void updateStrikeCount() {
+        TextView t = (TextView) findViewById(R.id.strikeCount);
+        t.setText(Integer.toString(strike_count));
+    }
+
+    private void updateBallCount() {
+        TextView t = (TextView) findViewById(R.id.ballCount);
+        t.setText(Integer.toString(ball_count));
+    }
+
+    private void updateOutCount() {
+        TextView t = (TextView) findViewById(R.id.outCount);
+        t.setText(Integer.toString(out_count));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnStrike:
+                // batter is out if strike_count ==3
+                strike_count++;
+                updateStrikeCount();
+                if (strike_count == 3) {
+                    // Builder is an inner class so we have to qualify it
+                    // with its outer class: AlertDialog
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    //builder.setTitle("Dialog box");
+                    builder.setMessage("BATTER OUT!!!");
+                    addOuts();
+                    builder.setCancelable(false);
+                    builder.setPositiveButton("Bummer.", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            strike_count = 0;
+                            ball_count = 0;
+                            // Note, you have to call update count here because.
+                            //   the call builder.show() below is non blocking.
+                            updateStrikeCount();
+                            updateBallCount();
+                        }
+
+                    });
+                    builder.show();
+                }
+                break;
+            case R.id.btnBall:
+                ball_count++;
+                updateBallCount();
+                if (ball_count == 4) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    //builder.setTitle("Dialog box");
+                    builder.setMessage("Batter walks.");
+                    builder.setCancelable(false);
+                    builder.setPositiveButton("Okay.", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            strike_count = 0;
+                            ball_count = 0;
+                            // Note, you have to call update count here because.
+                            //   the call builder.show() below is non blocking.
+                            updateStrikeCount();
+                            updateBallCount();
+
+                        }
+                    });
+                    builder.show();
+
+                }
+                break;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                viewAbout();
+                return true;
+            case R.id.action_reset:
+                strike_count=0;
+                ball_count=0;
+                updateStrikeCount();
+                updateBallCount();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void viewAbout() { //Unnecessary for the scope of this project, but will be useful in the future
+        Intent intent = new Intent(this, About.class);
+        startActivity(intent);
+    }
+
+    public void addOuts(){
+        //Saving the outs in the shared pref folder.
+        out_count++;
+        SharedPreferences prefs = this.getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("totalOuts", out_count);
+        editor.commit();
+        updateOutCount();
+    }
+
+    public void readOuts(){
+        SharedPreferences prefs = this.getSharedPreferences("myPrefsKey", Context.MODE_PRIVATE);
+        out_count = prefs.getInt("totalOuts", 0); //0 is the default value
+    }
+
+}
